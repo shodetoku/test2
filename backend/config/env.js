8ecaf84d6a82bf70c3bc95f9349d578dc3a19aec
Erickson <rixon.code@gmail.com>
2026-02-20 13:38:52 +0800

    Feat(config): Update environment variables to include database URIs for PARMS, IBMS, and HRMS

--- backend/config/env.js
+++ backend/config/env.js
@@ -5,5 +5,7 @@ config({path: `.env.${process.env.NODE_ENV || 'development'}.local`})
 export const { 
     PORT, 
     NODE_ENV,
-    DB_URI
+    PARMS_DB_URI,
+    IBMS_DB_URI,
+    HRMS_DB_URI
 } = process.env
